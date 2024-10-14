c5915e8b60cd959527e72ebf6041693ee34bc61e
Nils-dev-Mertens <nilsm2950@gmail.com>
2024-10-15 01:02:46 +0200

    added graphics

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 import si from "systeminformation";
 si.cpu()
-  .then(data => console.log(data))
+  .then(data => console.log(data.brand))
   .catch(error => console.error(error));
+si.graphics()
+  .then(data => console.log(data.controllers[0].model))
+  .catch(error => console.error(error));
+ 
