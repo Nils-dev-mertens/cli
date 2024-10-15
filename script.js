0f697efd58a366d9c0eccd5ea2389eeb9072416c
Nils-dev-mertens <nilsm2950@gmail.com>
2024-10-15 11:44:12 +0200

    added battery en os support

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 import si from "systeminformation";
-import fs from "fs"
+import fs from "fs/promises"
 si.cpu()
   .then(data => console.log(`CPU => ${data.brand}`))
   .catch(error => console.error(error));
 si.graphics()
   .then(data => console.log(`GPU => ${data.controllers[0].model}`))
   .catch(error => console.error(error));
- 
+si.osInfo()
+  .then(data => console.log(`OS => ${data.platform}`))
+  .catch(error => console.error(error));
+si.battery()
+  .then(data => console.log(`BATTERY => ${data.percent}, ${data.maxCapacity}mah`))
+  .catch(error => console.error(error));
