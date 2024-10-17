4ba85aed32d3b7c9ccec6980790f46550122b88e
Nils-dev-mertens <nilsm2950@gmail.com>
2024-10-17 10:17:51 +0200

    changed the name of script.js and added fucntion to write a json file with data from the pc

--- getdata.js
+++ getdata.js
@@ -1,10 +1,10 @@
 import si from "systeminformation";
 import fs from "fs/promises"
 let cpu = await si.cpu()
-  .then(data => {return data.brand})
+  .then(data => {return data})
   .catch(error => console.error(error));
 let gpu = await si.graphics()
-  .then(data => {return data.controllers[0].model})
+  .then(data => {return data.controllers[0]})
   .catch(error => console.error(error));
 let os = await si.osInfo()
   .then(data => {return data})
@@ -16,3 +16,4 @@ const arr = [cpu, gpu, os, battery];
 arr.forEach(element => {
   console.log(element);
 });
+fs.writeFile("data.json", JSON.stringify(arr, null, 2));
