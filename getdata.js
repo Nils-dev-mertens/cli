e68eec080bdd1dc9b667b3f873e222183660c5d1
Nils-dev-Mertens <nilsm2950@gmail.com>
2024-10-18 02:29:41 +0200

    added code to get everything and put it in a different json file

--- getdata.js
+++ getdata.js
@@ -1,25 +1,26 @@
 import si from "systeminformation";
 import fs from "fs/promises";
 export default async function createdata() {
-  let cpu = await si.cpu()
+const cpu = await si.cpu()
   .then(data => {return data})
   .catch(error => console.error(error));
-let gpu = await si.graphics()
+const gpu = await si.graphics()
   .then(data => {return data.controllers[0]})
   .catch(error => console.error(error));
-let os = await si.osInfo()
+const os = await si.osInfo()
   .then(data => {return data})
   .catch(error => console.error(error));
-let battery = await si.battery()
+const battery = await si.battery()
   .then(data => {return data})
   .catch(error => console.error(error));
-  // let network = await si.networkInterfaces()
-  // .then(data => {return data})
-  // .catch(error => console.error(error));
-const arr = [cpu, gpu, os, battery];
-arr.forEach(element => {
-  console.log(element);
-});
+const network = await si.networkInterfaces()
+  .then(data => {return data})
+  .catch(error => console.error(error));
+const all = await si.getAllData()
+  .then(data => {return data})
+  .catch(error => console.error(error));
+const arr = [cpu, gpu, os, battery, network];
 fs.writeFile("data.json", JSON.stringify(arr, null, 2));
+fs.writeFile("all.json", JSON.stringify(all, null, 2));
 }
 createdata();
