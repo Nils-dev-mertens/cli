60f68674eda9fb48a61e01d532df7de15383f5eb
Nils-dev-Mertens <nilsm2950@gmail.com>
2024-10-21 01:16:20 +0200

    added fucntions for arguments and read from the console

--- ui.js
+++ ui.js
@@ -1,3 +1,20 @@
 import pcdata from "./getdata.js";
+import readline from 'node:readline';
 import data from "./data.json" assert {type : "json"};
 console.log(data);
+const args = process.argv.slice(2); // Slice to ignore the first two default args
+if (args.length > 0) {
+    args.forEach((arg, index) => {
+        console.log(`Argument ${index + 1}: ${arg}`);
+    });
+} else {
+    console.log('No arguments provided.');
+}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,});
+
+rl.question(`new user?`, answer => {
+    console.log(`${answer}`);
+    rl.close();
+});
